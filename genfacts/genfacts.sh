35a23d5d166292d09f2a0b7875806fe10f2ff3cd
tanghao <tanghaoth90@gmail.com>
2017-10-14 15:22:05 +0800

    combobj-3-3

--- genfacts/genfacts.sh
+++ genfacts/genfacts.sh
@@ -5,5 +5,5 @@ for subject in antlr bloat chart eclipse fop luindex lusearch pmd xalan; do
 	echo "************************"
 	echo "Running ${subject}"
 	echo "************************"
-	./doop -a context-insensitive -i jars/dacapo/${subject}.jar --dacapo --id souffle-${subject} --souffle --souffle-jobs 12
+	./doop -a context-insensitive -i jars/dacapo/${subject}.jar --dacapo --id souffle-${subject} --souffle --souffle-jobs 8
 done
