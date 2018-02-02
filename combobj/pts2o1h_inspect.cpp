6fea6199631f3cae87ac230c9080823fa42d884b
tanghao <tanghaoth90@gmail.com>
2018-02-02 14:35:43 +0800

    extract varpointsto

--- combobj/pts2o1h_inspect.cpp
+++ combobj/pts2o1h_inspect.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include "souffle/SouffleInterface.h"
+#include "souffle/CompiledRamRecord.h"
 
 using namespace souffle;
 
@@ -10,49 +11,40 @@ void error(std::string txt) {
 	exit(1);
 }
 
-std::unordered_map<std::string, std::string[3]> ctx_mapping, hctx_mapping;
-
 int main(int argc, char **argv) {
+	std::cout << CTX_LEN << " " << HCTX_LEN << "\n";
 	if (SouffleProgram *prog = ProgramFactory::newInstance("pts2o1h_genclass")) {
 		std::cout << "successfully loaded!\n";
 		prog->loadAll(argv[1]);
 		prog->run();
+		const SymbolTable& progSymTable = prog->getSymbolTable();
 		//prog->printAll();
 		for (auto rel : prog->getAllRelations()) {
 			std::cout << rel->getName() << " " << rel->getSignature() << "\n";
 		}
-		size_t arity_ctx, arity_hctx;
-		if (Relation *rel_unfoldedcontext = prog->getRelation("UnfoldedContext")) {
-			arity_ctx = rel_unfoldedcontext->getArity();
-			for (auto &t : *rel_unfoldedcontext) {
-				size_t ctx_id = t[0];
-				std::string ctx_ss[arity_ctx-1];
-				for (size_t i = 1; i < arity_ctx; i++) {
-					ctx_ss[i-1] = t[i];
-				}
-			}
-		}
-		if (Relation *rel_unfoldedhcontext = prog->getRelation("UnfoldedHContext")) {
-			arity_hctx = rel_unfoldedhcontext->getArity();
-			for (auto &t : *rel_unfoldedhcontext) {
-				size_t hctx_id = t[0];
-				std::string hctx_ss[arity_hctx-1];
-				for (size_t i = 1; i < arity_hctx; i++) {
-					hctx_ss[i-1] = t[i];
-				}
-			}
-		}
 		if (Relation *rel_varpointsto = prog->getRelation("VarPointsTo")) {
-			size_t arity_varpointsto = rel_varpointsto->getArity();
-			std::cout << "arity of varpointsto: " << arity_varpointsto << "\n";
+			//size_t arity_varpointsto = rel_varpointsto->getArity();
+			//std::cout << "arity of varpointsto: " << arity_varpointsto << "\n";
+			size_t cnt = 0;
 			for (auto &t : *rel_varpointsto) {
-				std::string fact[arity_varpointsto+arity_ctx+arity_hctx-2];
-				size_t hctx_id = t[0], ctx_id = t[2];
-				//std::string myvalue = rel_varpointsto->getSymbolTable().resolve(t[1]), myvar = rel_varpointsto->getSymbolTable().resolve(t[3]);
-				fact[arity_hctx] = rel_varpointsto->getSymbolTable().resolve(t[1]);
-				fact[arity_varpointsto+arity_ctx+arity_hctx-3] = rel_varpointsto->getSymbolTable().resolve(t[3]);
+				std::string fact[HCTX_LEN+CTX_LEN+2];
+				ram::Tuple<RamDomain,HCTX_LEN> hctx = unpack<ram::Tuple<RamDomain,HCTX_LEN>>(t[0]);
+				for (size_t i = 0; i < HCTX_LEN; i++)
+					fact[i] = progSymTable.resolve(hctx[i]);
+				fact[HCTX_LEN] = progSymTable.resolve(t[1]);
+				ram::Tuple<RamDomain,CTX_LEN> ctx = unpack<ram::Tuple<RamDomain,CTX_LEN>>(t[2]);
+				for (size_t i = 0; i < CTX_LEN; i++)
+					fact[HCTX_LEN+i+1] = progSymTable.resolve(ctx[i]);
+				fact[HCTX_LEN+CTX_LEN+1] = progSymTable.resolve(t[3]);
 				//std::cout << hctx_id << "-" << myvalue << "-" << ctx_id << "-" << myvar << "\n";
 				// hash: consider the relation name!!
+				//fact[0] = unpack
+				if (cnt++ < 10) {
+					std::cout << fact[0] << std::endl;
+					for (size_t i = 1; i < HCTX_LEN+CTX_LEN+2; i++)
+						std::cout << " " << fact[i] << std::endl;
+					std::cout << std::endl;
+				}
 			}
 		} else {
 			error("cannot find relation VarPointsTo");
