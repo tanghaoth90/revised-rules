653e9d39ac1114ef48eb724481d8e2ddb3e1b5cf
tanghao <tanghaoth90@gmail.com>
2018-03-05 22:46:14 +0800

    gen datalog for neighbor extraction

---
+++ neighbor/gendl_extractneighbor.py
@@ -0,0 +1,19 @@
+import os
+fact_dir = "test"
+with open("extract_neighbor.dl", "w") as dl_script:
+	dl_script.write(".type String\n")
+	dl_script.write(".decl Center(c:String)\n")
+	dl_script.write("Center(\"<The Center>\").\n")
+	for filename in filter(lambda x:os.path.splitext(x)[1]=='.facts', os.listdir(fact_dir)):
+		relname = os.path.splitext(filename)[0]
+		with open(fact_dir+"/"+filename, "r") as fact_file:
+			arity = len(fact_file.readline().split("\t"))
+#			print "%s %d" % (relname, arity)
+			arg_defs = reduce((lambda a,b: a+", "+b), map(lambda x:("p%d:String"%x), range(arity)))
+			dl_script.write(".decl %s(%s)\n" % (relname, arg_defs))
+			dl_script.write(".decl %s_Nbr(%s)\n" % (relname, arg_defs))
+			dl_script.write(".input %s(IO=\"file\", filename=\"%s.facts\", delimiter=\"\\t\")\n" % (relname, relname))
+			for ec in range(arity):
+				args = reduce((lambda a,b: a+","+b), map(lambda x:("p%d"%x) if x!=ec else "c", range(arity)))
+				dl_script.write("%s_Nbr(%s) :- Center(c), %s(%s).\n" % (relname, args, relname, args))
+			dl_script.write(".output %s_Nbr(IO=stdout)\n" % relname)
--- neighbor/gendl_findneighbor.py
+++
@@ -1,7 +0,0 @@
-import os
-fact_dir=r'test'
-for filename in filter(lambda x:os.path.splitext(x)[1]=='.facts', os.listdir(fact_dir)):
-	print filename
-	relname = os.path.splitext(filename)[0]
-	print relname
-	
