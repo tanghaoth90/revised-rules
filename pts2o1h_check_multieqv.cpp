b1061e6ce583adfb20821a2d7490bbe6b0a23ca6
tanghao <tanghaoth90@gmail.com>
2018-03-13 21:52:04 +0800

    file output into a class

--- pts2o1h_check_multieqv.cpp
+++ pts2o1h_check_multieqv.cpp
@@ -62,67 +62,54 @@ void init_hashval_of_all_symbols(const SymbolTable &symTable) {
 }
 
 // TODO: obtain three containers in a class
-// TODO: counter and output info in a class
 
-std::unordered_map<RamDomain, size_t> counter; // # of facts that the element appears
-
-void init_mapping_tables() {
-	counter.clear();
-}
-
-int global_counter = 0;
-
-void init_counter() {
-	global_counter = 0;
-}
-
-/*
-void output_fact_info(RamDomain vec[], size_t rel_arity, const SymbolTable& progSymTable, size_t fact_hashval) {
-	if (global_counter++ > 10) return;
-	std::cout << vec[0];
-	for (size_t i = 1; i < rel_arity; i++)
-		std::cout << " " << vec[i];
-}
-*/
+class output_processor {
+	FILE* outfile;
+public:
+	output_processor(const char * filename) {
+		outfile = fopen(filename, "w");
+	}
+	void output_fact_info(RamDomain vec[], size_t rel_arity) {
+		fprintf(outfile, "%d", (int)vec[0]);
+		for (int i = 1; i < rel_arity; i++)
+			fprintf(outfile, "\t%d", (int)vec[i]);
+		fprintf(outfile, "\n");
+	}
+	~output_processor() {
+		fclose(outfile);
+	}
+};
 
 int main(int argc, char **argv) {
 	// argv[1] : input
 	// argv[2] : replace_file
-	// std::cout << CTX_LEN << " " << HCTX_LEN << "\n";
 	if (SouffleProgram *prog = ProgramFactory::newInstance("pts2o1h_genclass")) {
-		std::cout << "successfully loaded!\n";
+		std::cout << "pts2o1h_genclass successfully loaded!\n";
 		prog->loadAll(argv[1]);
 		prog->run();
 		//prog->printAll();
 		const SymbolTable& progSymTable = prog->getSymbolTable();
 		init_hashval_of_all_symbols(progSymTable);
-		for (auto rel : prog->getAllRelations()) {
-			std::cout << rel->getName() << " " << rel->getSignature() << "\n";
-		}
-		init_mapping_tables();
-		std::string rel_name;
-		rel_name = "CallGraphEdge";
-		init_counter();
-		FILE* outfile_callgraphedge = fopen("CallGraphEdge.log", "w");
+		//for (auto rel : prog->getAllRelations()) {
+		//	std::cout << rel->getName() << " " << rel->getSignature() << "\n";
+		//}
+		std::string rel_name = "CallGraphEdge";
 		if (Relation *rel = prog->getRelation(rel_name)) {
 			size_t rel_hashval = std::hash<std::string>()(rel_name);
 			size_t arity_ori = 4;
-			size_t attr_width[4] = {2, 0, 2, 0};
+			size_t attr_width[arity_ori] = {2, 0, 2, 0};
 			size_t rel_arity = 0;
 			for (size_t i = 0; i < arity_ori; i++)
 				rel_arity += attr_width[i] == 0 ? 1 : attr_width[i];
+			output_processor out((rel_name+".log").c_str());
 			for (auto &t : *rel) {
 				RamDomain vec[rel_arity];
 				unfold(vec, t, arity_ori, attr_width);
-				fprintf(outfile_callgraphedge, "%d", (int)vec[0]);
-				for (int i = 1; i < rel_arity; i++)
-					fprintf(outfile_callgraphedge, "\t%d", (int)vec[i]);
-				fprintf(outfile_callgraphedge, "\n");
+				out.output_fact_info(vec, rel_arity);
 			}
 		} 
 		else 
 			error("cannot find relation " + rel_name);
-		fclose(outfile_callgraphedge);
 	}
 	else {
 		error("cannot find program oh");
