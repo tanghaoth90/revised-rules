9912c97c129275e26a3b3c96b13154c1ca914aee
tanghao <tanghaoth90@gmail.com>
2018-03-29 01:38:27 +0800

    add cal_eqv function

--- multi_derivation.py
+++ multi_derivation.py
@@ -2,9 +2,6 @@ import csv, sys
 from collections import Counter
 import itertools
 
-db_unfold = sys.argv[1] + "/"
-#replace1 = sys.argv[2]
-
 def extract_by_indices(t, indices):
 	return tuple([t[i] for i in indices])
 
@@ -21,9 +18,9 @@ def load_rel_by_key(rel_set, k_indices, v_indices):
 	rel_dict = {}
 	for fact in rel_set:
 		key = extract_by_indices(fact, k_indices)
-		value = extract_by_indices(fact, v_indices)
+		#value = extract_by_indices(fact, v_indices)
 		if key not in rel_dict: rel_dict[key] = []
-		rel_dict[key].append(value)
+		rel_dict[key].append(fact)
 	return rel_dict
 			
 def cal_eqv(rel_set, k_indices, v_indices):
@@ -47,41 +44,37 @@ def cal_eqv(rel_set, k_indices, v_indices):
 	reduced_facts = [fact for fact in rel_set if extract_by_indices(fact, k_indices) not in notrep]
 	return len(reduced_facts), len(rel_set), float(len(reduced_facts)) / len(rel_set)
 
-cge_set = load_rel_from_file('CallGraphEdge.csv')
-print 'CGE', cal_eqv(cge_set, [0,1,3,4], [2,5])
-
-vpt_set = load_rel_from_file('VarPointsTo.csv')
-print 'VPT', cal_eqv(vpt_set, [0,2,3], [1,4])
-
-storehif_set = load_rel_from_file('StoreHeapInstanceField.csv')
-print 'SHIF', cal_eqv(storehif_set, [1,3,4], [0,2,5])
-
-'''
-cge_set = load_rel_from_file('CallGraphEdge.csv')
-tpt_set = load_rel_from_file('ThrowPointsTo.csv')
-
-cge_dict = load_rel_by_key(cge_set, range(3,6), range(0,3))
-tpt_dict = load_rel_by_key(tpt_set, range(2,5), range(0,2))
-
-insth_list = [y+x for k in cge_dict if k in tpt_dict for x in cge_dict[k] for y in tpt_dict[k]]
-multi_count = Counter(insth_list).values()
-print len(insth_list) #, sum(multi_count)
-print Counter(multi_count)
-
-cal_eqv(set(insth_list), [0,2,3], [1,4])
-'''
-'''
-storehif_sss = load_rel_from_file('StoreHeapInstanceField.csv')
-vpt_sss = load_rel_from_file('VarPointsTo.csv')
-
-storehif_set = load_rel_by_key(storehif_sss, range(3,6), range(0,3))
-vpt_set = load_rel_by_key(vpt_sss, range(2,5), range(0,2))
-
-iftp_set =	[x+y for k in storehif_set if k in vpt_set for x in storehif_set[k] for y in vpt_set[k]]
-print iftp_set[0]
-multi_count = Counter(iftp_set).values()
-print sum(multi_count)
-freq_count = Counter(multi_count)
-print freq_count
-print sum(freq_count.values())
-'''
+if __name__ == "__main__":
+	db_unfold = sys.argv[1] + "/"
+	#replace1 = sys.argv[2]
+	runmode = sys.argv[2]
+	if runmode == '1':
+		cge_set = load_rel_from_file('CallGraphEdge.csv')
+		print 'CGE', cal_eqv(cge_set, [0,1,3,4], [2,5])
+		tpt_set = load_rel_from_file('ThrowPointsTo.csv')
+		print 'TPT', cal_eqv(tpt_set, [0,2,3], [1,4])
+		cge_dict = load_rel_by_key(cge_set, range(3,6), range(0,3))
+		tpt_dict = load_rel_by_key(tpt_set, range(2,5), range(0,2))
+		insth_list = [y[:2]+x[:3] for k in cge_dict if k in tpt_dict for x in cge_dict[k] for y in tpt_dict[k]]
+		insth_set = set(insth_list)
+		multi_count = Counter(insth_list).values()
+		print 'INSTH list len = ', len(insth_list) #, sum(multi_count)
+		print 'INSTH set len = ', len(insth_set)
+		print Counter(multi_count)
+		print cal_eqv(insth_set, [0,2,3], [1,4])
+	elif runmode == '2':	
+		storehif_set = load_rel_from_file('StoreHeapInstanceField.csv')
+		print 'SHIF', cal_eqv(storehif_set, [1,3,4], [0,2,5])
+		vpt_set = load_rel_from_file('VarPointsTo.csv')
+		print 'VPT', cal_eqv(vpt_set, [0,2,3], [1,4])
+		storehif_dict = load_rel_by_key(storehif_set, range(3,6), range(0,3))
+		vpt_dict = load_rel_by_key(vpt_set, range(2,5), range(0,2))
+		iftp_list =	[y[:2]+x[:3] for k in storehif_dict if k in vpt_dict for x in storehif_dict[k] for y in vpt_dict[k]]
+		iftp_set = set(iftp_list)
+		multi_count = Counter(iftp_list).values()
+		print 'IFTP list len = ', len(iftp_list) #, sum(multi_count)
+		print 'IFTP set len = ', len(iftp_set)
+		print Counter(multi_count)
+		print cal_eqv(iftp_set, [0,3], [1,2,4])
+		print cal_eqv(iftp_set, [3,4], [0,1,2])
+	
